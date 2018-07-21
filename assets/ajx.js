35207074a5289c8279a734c90f6c81c7d404a856
jrkerr2 <johnrkerr@hotmail.com>
2018-07-21 10:11:50 -0400

    in class save

--- assets/ajx.js
+++ assets/ajx.js
@@ -1,7 +1,9 @@
-var apiKey = Z8lBjCboNSl3SWH8t4ewhD5ZTKp82tJQ;
+var apiKey = Z8lBjCboNSl3SWH8t4ewhD5ZTKp82tJQ; //giphy API key for John Kerr
 
 var topics = ["mouse", "woodchuck", "baboon", "kangaroo", "hyena"];
 
-var apiURL = "";
+var apiURL = `https://cnn.com/articles/?${searchString}`;
 
 var searchString = "";
+
+//use joins??? in query strings
